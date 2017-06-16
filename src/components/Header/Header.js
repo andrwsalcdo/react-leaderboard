e9dc549e1f57f495cf8a2593c54194a13cf3f9f8
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-16 00:43:47 -0400

    add Bootstrap, Header component with styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => (
+  <nav className="navbar navbar-default">
+     <div className="container-fluid">
+        <div className="navbar-header">
+            <h1 className="Header-title">Leaderboard</h1>
+            <h4 className="Header-subtitle">React.js</h4>
+        </div>
+     </div>
+   </nav>
+)
+
+export default Header;  
