f311e10d12fd602c0083ccf11c26e4e565aa0d0b
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-17 11:49:49 -0400

    fix: Leaderboard comp layout for future components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css'
 
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer';
+// import Leaderboard from './components/Body/Leaderboard';
 
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
     return (
       <div>
           <Header />
-          {/*// <Body/Table/Board /> */}
+          {/*<Leaderboard />*/}
           <Footer /> 
       </div>
     );
--- src/components/Body/Leaderboard.js
+++ src/components/Body/Leaderboard.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+// import Title from './Title'; 
+// import Tableheader from './Tableheader'; 
+// import Userlist from './Userlist'; 
+
+
 const LeaderBoard = () => (
-    <Title  /> 
-    <Tableheader />
-    <Userlist />
+    <div className="container">
+        {/*<Title  /> */}
+        {/*// <Tableheader />
+        // <Userlist />*/}
+    </div>    
 )
 
 export default LeaderBoard; 
