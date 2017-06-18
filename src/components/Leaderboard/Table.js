cefd66258f6b0736fb904a9bfa014605223b7259
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-18 18:32:51 -0400

    add User component draft. todo: Api call

--- src/components/Leaderboard/Table.js
+++ src/components/Leaderboard/Table.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import TableHeader from './Tableheader'; 
+import User from './User'; 
 
 const Table = () => (
     <table className="table table-striped table-bordered">
         <TableHeader />
         {/*table body componenet: userlist*/}
+            <User />
     </table>
 )
 
---
+++ src/components/Leaderboard/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const User = () => (
+    <tr>
+        <td className="idcol">count</td>
+        <td>
+            <a href="fcc link" target="_blank"></a>
+            <img src="" alt="User's Avatar Image" className="userimg" />
+            <span>username</span>
+        </td>
+        <td className="pointscol">points</td>
+        <td className="pointscol">points</td>        
+    </tr>
+)
+
+export default User 
