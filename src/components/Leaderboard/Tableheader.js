df34e91bb7f1ae7d7491f405a57f356a64a70a73
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-19 13:03:32 -0400

    add links for onClick to display users by recent and top points. more styling needed

--- src/components/Leaderboard/Tableheader.js
+++ src/components/Leaderboard/Tableheader.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Tableheader = () => (
+const Tableheader = (props) => (
     <thead> 
         <tr id="colheaders" className="top100">
             <th className="idcol">#</th>
             <th>Camper Name</th>
-            <th className="30days">Points in past 30 days</th>
-            <th className="allTime">All time points</th>
+            <th className="30days">
+              <a onClick={props.toggleRecentScores}>Points in past 30 days</a>
+            </th>
+            <th className="allTime">
+               <a onClick={props.toggleTopScores}>All time points</a>
+            </th>
         </tr>
     </thead>
 )
