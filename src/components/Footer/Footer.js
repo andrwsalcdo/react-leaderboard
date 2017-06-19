b6335a14ef3aa51fbb67eca8073d6e450ebf8c29
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-19 13:05:59 -0400

    add id footer to enable syling changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Footer = () => (
-    <div className="container-fluid">
-        <div className="col-md-12">
+    <div id="footer" className="container-fluid">
+      <div className="row">
+        <div className="col-lg-12">
             <div className="Footer">
                 <p className="text-center">Made by Andrew Salcedo</p>
             </div>  
         </div>
+      </div>
     </div>
 )
 
