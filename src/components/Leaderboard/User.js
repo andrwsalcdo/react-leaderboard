ee3f27d80cb294faf93691bc1081958eb6971842
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-19 13:22:19 -0400

    Alt text to include username

--- src/components/Leaderboard/User.js
+++ src/components/Leaderboard/User.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const User = (props) => (
-    <tr>
-        <td className="idcol">{props.rank}</td>
-        <td>
-            <a href={"https://www.freecodecamp.com/"+props.data.username} target="_blank">
-            <img src={props.data.img} alt="User's Avatar" className="userimg" />
-            <span>{props.data.username}</span>
-            </a>
-        </td>
-        <td className="pointscol">{props.data.recent}</td>
-        <td className="pointscol">{props.data.alltime}</td>        
-    </tr>
-)
+const User = (props) =>  
+        <tr>
+            <td className="idcol">{props.rank}</td>
+            <td>
+                <a href={"https://www.freecodecamp.com/"+props.data.username} target="_blank">
+                    <img src={props.data.img} alt={props.data.username + "'s avatar."} className="userimg" />            
+                    <span>{props.data.username}</span>
+                </a>
+            </td>
+            <td className="pointscol">{props.data.recent}</td>
+            <td className="pointscol">{props.data.alltime}</td>        
+        </tr>
 
-export default User 
+export default User; 
