3aecc3392594ebf2ca7210068e756989c83cf15a
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-19 13:01:27 -0400

    add Leaderboard component. maps and displays User, displays the Table with all nested components

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,15 +1,43 @@
 import React from 'react';
 
+//import child components
 import Title from './Title'; 
-import Table from './Table'; 
+import Tableheader from './Tableheader'; 
+import User from './User'; 
 
 
+const Leaderboard = (props) => {
+    let rank = 0; 
+    const rows = props.users; 
+    const userList = rows.map((users, index) => {
+        rank++; 
+        return (
+            //mapping over the User component to display them in the <tbody>
+            //Key makes each User unique. 
+            <User key={index} data={users} rank={rank} />
+        )
+    }); 
+    return ( 
+                     <div className="container">        
+                        <Title />
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <table className="table table-striped table-bordered">
+                                    <Tableheader 
+                                        //pass on the click handler props to this child component
+                                        // to display recent and top points
+                                        toggleTopScores={props.onClickTop} 
+                                        toggleRecentScores={props.onClickRecent} 
+                                    />
+                                    <tbody>
+                                        {userList}
+                                     </tbody>
+                                 </table>
+                             </div>
+                         </div>
+                     </div>    
+    );
 
-const LeaderBoard = () => (
-    <div className="container">
-        <Title  /> 
-        <Table />
-    </div>    
-)
+}
 
-export default LeaderBoard; 
+export default Leaderboard; 
