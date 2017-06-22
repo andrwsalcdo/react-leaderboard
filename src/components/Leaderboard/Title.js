2744bed5c891f41de2d3b8799bc71136050210ac
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-22 15:54:38 -0400

    styling: Leaderboard Title. add SASS nesting

--- src/components/Leaderboard/Title.js
+++ src/components/Leaderboard/Title.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 const Title = () => (
-    <div className="row">
+    <div className="row text-center">
         <div className="col-md-12">
             <div id="leaderboard-Title">
-                <h3>Leaderboard</h3>
+                <h3>Free Code Camp Leaderboard</h3>
             </div>
         </div>
     </div>
