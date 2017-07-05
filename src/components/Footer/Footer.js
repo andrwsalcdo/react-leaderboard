ca8ea905fb3a159522422131637b854c045e7e2a
Andrew Salcedo <asalcedo07@gmail.com>
2017-07-04 21:49:02 -0400

    add: react-icons to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import FaGithub from 'react-icons/lib/fa/github';
+
 const Footer = () => (
-    <div id="footer" className="container-fluid">
-      <div className="row">
-        <div className="col-lg-12">
-            <div className="Footer">
-                <p className="text-center">Made by Andrew Salcedo</p>
-            </div>  
-        </div>
-      </div>
-    </div>
+    <div id="footer" className="text-center">
+        <p >Made by Andrew Salcedo
+        <span> 
+            <a href="https://github.com/andrwsalcdo/react-leaderboard">
+                <FaGithub size={25} color="black" /> 
+            </a>        
+        </span>
+        </p>
+        
+    </div>  
 )
 
 export default Footer; 
